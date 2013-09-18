1212307a7e81ecd99107b20f5ba05c3569fa2264
Nick Cammarata <nick@nickcammarata.com>
2013-09-17 22:58:32 -0700

    cleaned up jquery calls

--- turtle.js
+++ turtle.js
@@ -6,13 +6,15 @@ function blockAndDisplay() {
         message.remove();
     } else if (message.length == 0) {
         feed.hide();
-        message = $('<h1>');
-        message.attr('id', 'distracted');
-        message.html("Don't get distracted by Facebook!");
-        message.css('font-size', '32px');
-        message.css('font-family', "'Helvetica Neue', Helvetica, Arial, 'lucida grande', tahoma, verdana, arial, sans-serif");
-        message.css('position', 'relative');
-        message.css('top', '100px');
+        message = $('<h1>')
+        .attr({ id: 'distracted' })
+        .html("Don't get distracted by Facebook!")
+        .css({
+          'font-size': '32px',
+          'font-family': "'Helvetica Neue', Helvetica, Arial, 'lucida grande', tahoma, verdana, arial, sans-serif",
+          position: 'relative',
+          top: '100px'
+        })
         $('[data-location=maincolumn]').append(message);
     }
 }
