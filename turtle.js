8124e0e8c5725f839b07f6c7e8c1dfd99bde3706
Neal Wu <neal@nealwu.com>
2013-09-18 00:17:47 -0400

    Code is really nice now

--- turtle.js
+++ turtle.js
@@ -5,42 +5,25 @@ function getFeed() {
 }
 
 function blockAndDisplay() {
-    $(document).ready(function() {
-        var feed = getFeed();
-
-        if (feed.length > 0) {
-            feed.hide();
-
-            var container = $('#globalContainer');
-            var message = $('<h1>');
-            message.attr('id', 'distraction_message');
-            message.html("Don't get distracted by Facebook!");
-            message.css('font-size', '36px');
-            message.css('font-family', "'Helvetica Neue', Helvetica, Arial, 'lucida grande', tahoma, verdana, arial, sans-serif");
-            message.css('position', 'absolute');
-            message.css('left', '20px');
-            message.css('top', '250px');
-            container.append(message);
-        }
-    });
-}
-
-function hideMessage() {
     $('#distraction_message').remove();
+    var feed = getFeed();
+
+    if (feed.length > 0) {
+        feed.hide();
+
+        var container = $('#globalContainer');
+        var message = $('<h1>');
+        message.attr('id', 'distraction_message');
+        message.html("Don't get distracted by Facebook!");
+        message.css('font-size', '36px');
+        message.css('font-family', "'Helvetica Neue', Helvetica, Arial, 'lucida grande', tahoma, verdana, arial, sans-serif");
+        message.css('position', 'absolute');
+        message.css('left', '20px');
+        message.css('top', '250px');
+        container.append(message);
+   }
 }
 
 if (document.URL.indexOf("facebook.com") !== -1) {
-    getFeed().hide();
-    blockAndDisplay();
-    var classes = $('body').attr('class');
-
-    setInterval(function() {
-        var newClasses = $('body').attr('class');
-
-        if (classes != newClasses) {
-            classes = newClasses;
-            hideMessage();
-            blockAndDisplay();
-        }
-    }, 200);
+    setInterval(blockAndDisplay, 250);
 }
