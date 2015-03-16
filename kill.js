9fa020b8f599d00fe4a592514b4b39b6823372b7
Victor Pontis <vpontis@gmail.com>
2015-03-16 04:27:44 -0400

    update the code to nicely display inspirational quotes

--- kill.js
+++ kill.js
@@ -1,32 +1,62 @@
+var quotesUrl = chrome.extension.getURL('quotes.json')
+var quote = ""
+$.getJSON(quotesUrl, function(json) {
+  var quotes = json.quotes
+  quote = quotes[Math.floor(Math.random()*quotes.length)]
+})
+
 function blockAndDisplay() {
-  var feed = $('[id^=topnews_main_stream], [id^=mostrecent_main_stream], [id^=pagelet_home_stream]');
-  var message = $('#distracted');
+  var feed = $('[id^=topnews_main_stream], [id^=mostrecent_main_stream], [id^=pagelet_home_stream]')
+  var optionsUrl = chrome.extension.getURL('options.html')
+
+  var elementsToRemove = [
+    '.ticker_stream',
+    '.ego_column',
+    '#pagelet_games_rhc',
+    '#pagelet_trending_tags_and_topics',
+    '#pagelet_canvas_nav_content'
+  ]
+  var message = $('#distracted')
 
   chrome.storage.sync.get({
     displayText: "Don't get distracted by Facebook"
   }, function(items) {
-    var messageText = items.displayText;
+    var messageText
+    if (quote.length) {
+      messageText = quote
+    } else {
+      messageText = items.displayText
+    }
 
     if (feed.length == 0) {
-      message.remove();
+      message.remove()
     } else if (message.length == 0) {
       var newMessage = $('<h1>')
         .attr('id', 'distracted')
         .text(messageText)
-        .css('font-size', '30px')
+        .css('font-size', '25px')
         .css('font-family', "'Helvetica Neue', Helvetica, Arial, 'lucida grande', tahoma, verdana, arial, sans-serif")
-        .css('position', 'relative')
-        .css('top', '75px');
-      $('[data-location=maincolumn]').append(newMessage);
+        .css('padding', '20px')
+        .css('border-top', 'solid rgb(231, 232, 235) 20px')
+      if ($('#distracted').length == 0) $('[data-location=maincolumn]').append(newMessage)
+
+      // var optionsLink = $('<a>')
+      //   .attr('id', 'options-link')
+      //   .attr('href', optionsUrl)
+      //   .attr('target', '_blank')
+      //   .text('Click here to change options for this extension')
+      //   .css('font-size', '15px')
+      //   .css('font-family', "'Helvetica Neue', Helvetica, Arial, 'lucida grande', tahoma, verdana, arial, sans-serif")
+      // if ($('#options-link').length == 0) {
+      //   $('#distracted').append('<br>').append(optionsLink)
+      // }
     }
 
-    feed.children().remove();
-    $('.ticker_stream').remove();
-    $('.ego_column').remove();
-    $('#pagelet_games_rhc').remove();
-    $('#pagelet_trending_tags_and_topics').remove();
-    $('#pagelet_canvas_nav_content').remove();
+    feed.children().hide()
+    for (var i in elementsToRemove) {
+      $(elementsToRemove[i]).hide()
+    }
   });
 }
 
-window.setInterval(blockAndDisplay, 100);
+window.setInterval(blockAndDisplay, 100)
