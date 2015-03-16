35224b7b0f06b39eceadec03ba5ad7898d5a350e
Victor Pontis <vpontis@gmail.com>
2015-03-16 04:58:49 -0400

    add image to container

--- kill.js
+++ kill.js
@@ -1,21 +1,25 @@
+var intervalId
 var quotesUrl = chrome.extension.getURL('quotes.json')
+var imageUrl = chrome.extension.getURL('images/vw-bus.jpeg')
 var quote = ""
 $.getJSON(quotesUrl, function(json) {
   var quotes = json.quotes
   quote = quotes[Math.floor(Math.random()*quotes.length)]
 })
 
-function blockAndDisplay() {
-  var feed = $('[id^=topnews_main_stream], [id^=mostrecent_main_stream], [id^=pagelet_home_stream]')
-  var optionsUrl = chrome.extension.getURL('options.html')
+var feedString = '[id^=topnews_main_stream], [id^=mostrecent_main_stream], [id^=pagelet_home_stream]'
+var elementsToRemove = [
+  '.ticker_stream',
+  '.ego_column',
+  '#pagelet_games_rhc',
+  '#pagelet_trending_tags_and_topics',
+  '#pagelet_canvas_nav_content'
+]
 
-  var elementsToRemove = [
-    '.ticker_stream',
-    '.ego_column',
-    '#pagelet_games_rhc',
-    '#pagelet_trending_tags_and_topics',
-    '#pagelet_canvas_nav_content'
-  ]
+function hideTimelineAndDisplayQuote() {
+  var optionsUrl = chrome.extension.getURL('options.html')
+  var feed = $(feedString)
+
   var message = $('#distracted')
 
   chrome.storage.sync.get({
@@ -31,14 +35,27 @@ function blockAndDisplay() {
     if (feed.length == 0) {
       message.remove()
     } else if (message.length == 0) {
+      var timelineReplacementContainer = $('<div>')
+        .attr('id', 'timeline-replacement-container')
+        .css('text-align', 'center')
+      if ($('#timeline-replacement-container').length == 0) $('[data-location=maincolumn]').append(timelineReplacementContainer)
+
       var newMessage = $('<h1>')
         .attr('id', 'distracted')
         .text(messageText)
         .css('font-size', '25px')
         .css('font-family', "'Helvetica Neue', Helvetica, Arial, 'lucida grande', tahoma, verdana, arial, sans-serif")
         .css('padding', '20px')
+        .css('text-align', 'left')
         .css('border-top', 'solid rgb(231, 232, 235) 20px')
-      if ($('#distracted').length == 0) $('[data-location=maincolumn]').append(newMessage)
+      if ($('#distracted').length == 0) $('#timeline-replacement-container').append(newMessage)
+
+      var image = $('<img>')
+        .attr('id', 'better-image')
+        .attr('src', imageUrl)
+        .attr('alt', 'A really nice picture!')
+        .css('width', '90%')
+      if ($('#better-image').length == 0) $('#timeline-replacement-container').append('<br>').append(image)
 
       // var optionsLink = $('<a>')
       //   .attr('id', 'options-link')
@@ -50,6 +67,14 @@ function blockAndDisplay() {
       // if ($('#options-link').length == 0) {
       //   $('#distracted').append('<br>').append(optionsLink)
       // }
+
+      // var unhideTimeline = $('<a>')
+      //   .attr('id', 'unhide-timeline')
+      //   .attr('onclick', 'displayTimeline();')
+      //   .text('Unhide timeline')
+      // if ($('#unhide-timeline').length == 0) {
+      //   $('#distracted').append('<br>').append(unhideTimeline)
+      // }
     }
 
     feed.children().hide()
@@ -59,4 +84,18 @@ function blockAndDisplay() {
   });
 }
 
-window.setInterval(blockAndDisplay, 100)
+function displayTimeline() {
+  window.clearInterval(intervalId)
+  $('#distracted').hide()
+
+  $(feedString).children().show()
+  for (var i in elementsToRemove) {
+    $(elementsToRemove[i]).show()
+  }
+}
+
+function hideTimeline() {
+  intervalId = window.setInterval(hideTimelineAndDisplayQuote, 100)
+}
+
+hideTimeline()
