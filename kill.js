329b97252e0e52b7f0df802cbca1945653ae7939
Neal Wu <neal@nealwu.com>
2020-04-05 16:55:00 -0700

    Update KillNewsFeed to support the new Facebook redesign and also to remove stories

--- kill.js
+++ kill.js
@@ -1,20 +1,5 @@
 function blockAndDisplay() {
     var feed = $('[id^=topnews_main_stream], [id^=mostrecent_main_stream], [id^=pagelet_home_stream]');
-    var message = $('#distracted');
-
-    if (feed.length == 0) {
-        message.remove();
-    } else if (message.length == 0) {
-        message = $('<h1>')
-            .attr('id', 'distracted')
-            .text("Don't get distracted by Facebook!")
-            .css('font-size', '30px')
-            .css('font-family', "'Helvetica Neue', Helvetica, Arial, 'lucida grande', tahoma, verdana, arial, sans-serif")
-            .css('position', 'relative')
-            .css('top', '75px');
-        $('[data-location=maincolumn]').append(message);
-     }
-
     feed.children().remove();
     $('.ticker_stream').remove();
     $('.ego_column').remove();
@@ -22,6 +7,10 @@ function blockAndDisplay() {
     $('#pagelet_trending_tags_and_topics').remove();
     $('#pagelet_canvas_nav_content').remove();
     $('#stories_pagelet_below_composer').remove();
+
+    $('[role=feed]').remove();
+    $('[aria-label=Stories]').remove();
+    $('[data-pagelet=Stories]').remove();
 }
 
 window.setInterval(blockAndDisplay, 100);
